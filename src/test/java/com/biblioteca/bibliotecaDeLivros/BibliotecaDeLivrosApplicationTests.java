8dd2e067af11923a1cf50c00939ff5ea31ddabc8
p3rsidh <persidh.pimenta@gmail.com>
2022-08-10 15:47:41 -0300

    Iniciando

---
+++ src/test/java/com/biblioteca/bibliotecaDeLivros/BibliotecaDeLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.bibliotecaDeLivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaDeLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
